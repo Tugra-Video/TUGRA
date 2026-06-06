8d0e107148f776d29017214fe4c71bf211e7e0cf
CRTY PUBG <info@crty-dev.com>
2026-06-06 06:29:54 +0300

    Fix class import mapping in __init__.py

--- tugra/__init__.py
+++ tugra/__init__.py
@@ -1,7 +1,7 @@
-# Copyright 2026 KAYRA. All rights reserved.d.
+# Copyright 2026 KAYRA. All rights reserved.
 from . import configs, distributed, modules
-from .image2video import TugraI2V
-from .speech2video import TugraS2V
-from .text2video import TugraT2V
+from .image2video import WanI2V as TugraI2V
+from .speech2video import WanS2V as TugraS2V
+from .text2video import WanT2V as TugraT2V
 from .textimage2video import TugraTI2V
-from .animate import TugraAnimate
+from .animate import WanAnimate as TugraAnimate
